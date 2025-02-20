8663c98c12394ce019aa9fa8cf42eec33cd20ff1
Soros Liu <soros.liu1029@gmail.com>
2025-02-20 22:49:50 +0800

    set description for debugger keymap

--- nvim/lua/plugins/debuger.lua
+++ nvim/lua/plugins/debuger.lua
@@ -1,49 +1,49 @@
 return {
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"rcarriga/nvim-dap-ui",
-		},
-		config = function()
-			local dap, dapui = require("dap"), require("dapui")
-			dapui.setup()
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function()
+      local dap, dapui = require("dap"), require("dapui")
+      dapui.setup()
 
-			vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
-			vim.keymap.set("n", "<Leader>db", dap.set_breakpoint, {})
-			vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+      vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, { desc = "Toggle breakpoint" })
+      vim.keymap.set("n", "<Leader>db", dap.set_breakpoint, { desc = "Set breakpoint" })
+      vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "Continue" })
 
-			dap.listeners.before.attach.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				dapui.close()
-			end
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
 
-			-- setup for new added dap
-		end,
-	},
-	{
-		"rcarriga/nvim-dap-ui",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-			"nvim-neotest/nvim-nio",
-		},
-		config = function()
-			require("lazydev").setup({
-				library = { "nvim-dap-ui" },
-			})
-		end,
-	},
-	{
-		"theHamsta/nvim-dap-virtual-text",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-		},
-	},
+      -- setup for new added dap
+    end,
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      require("lazydev").setup({
+        library = { "nvim-dap-ui" },
+      })
+    end,
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+  },
 }
