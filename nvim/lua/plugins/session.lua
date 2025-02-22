c764538155b3fe06606e5800d56f26a2ff3d7c5c
Soros Liu <soros.liu1029@gmail.com>
2025-02-22 17:39:41 +0800

    add zen mode for nvim

--- nvim/lua/plugins/session.lua
+++ nvim/lua/plugins/session.lua
@@ -1,24 +1,28 @@
 return {
-	"folke/persistence.nvim",
-	event = "BufReadPre", -- this will only start session saving when an actual file was opened
-	opts = {
-		-- add any custom options here
-	},
-	init = function()
-		vim.keymap.set("n", "<leader>qs", function()
-			require("persistence").load()
-		end, { desc = "Load the session for the current directory" })
+  "folke/persistence.nvim",
+  event = "BufReadPre", -- this will only start session saving when an actual file was opened
+  opts = {
+    -- add any custom options here
+  },
+  init = function()
+    -- remove Neo-tree from resotre
+    -- ref: https://github.com/folke/persistence.nvim/issues/80#issuecomment-2506424807
+    vim.opt.sessionoptions:remove("blank")
 
-		vim.keymap.set("n", "<leader>qS", function()
-			require("persistence").select()
-		end, { desc = "Select a session to load" })
+    vim.keymap.set("n", "<leader>qs", function()
+      require("persistence").load()
+    end, { desc = "Load the session for the current directory" })
 
-		vim.keymap.set("n", "<leader>ql", function()
-			require("persistence").load({ last = true })
-		end, { desc = "Load the last session" })
+    vim.keymap.set("n", "<leader>qS", function()
+      require("persistence").select()
+    end, { desc = "Select a session to load" })
 
-		vim.keymap.set("n", "<leader>qd", function()
-			require("persistence").stop()
-		end, { desc = "Stop Persistence => session won't be saved on exit" })
-	end,
+    vim.keymap.set("n", "<leader>ql", function()
+      require("persistence").load({ last = true })
+    end, { desc = "Load the last session" })
+
+    vim.keymap.set("n", "<leader>qd", function()
+      require("persistence").stop()
+    end, { desc = "Stop Persistence => session won't be saved on exit" })
+  end,
 }
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,18 +1,18 @@
-vim.cmd("set encoding=utf-8")
+vim.o.encoding = "utf-8"
 
 -- appearance related
-vim.cmd("set number")
-vim.cmd("set relativenumber")
-vim.cmd("set ruler")
-vim.cmd("set cursorline")
-vim.cmd("set cursorcolumn")
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.ruler = true
+vim.o.cursorline = true
+vim.o.cursorcolumn = true
 
 -- indent related
-vim.cmd("set autoindent")
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.o.autoindent = true
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
 
 vim.opt.termguicolors = true
 
@@ -24,4 +24,3 @@ vim.g.maplocalleader = "\\"
 -- :checkhealth
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
-
