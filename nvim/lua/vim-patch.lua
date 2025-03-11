8aacf1f31c4e5328fcb9269d910b3d1bcc760b05
Soros Liu <soros.liu1029@gmail.com>
2025-03-11 12:11:58 +0800

    tweak neo-tree read file for images

--- nvim/lua/vim-patch.lua
+++ nvim/lua/vim-patch.lua
@@ -1,15 +1,22 @@
--- open pdf files in external program
+-- open pdf/image files in external program
 vim.api.nvim_create_autocmd("BufReadCmd", {
-  pattern = "*.pdf",
+  pattern = { "*.pdf", "*.png", "*.jpg" },
   callback = function()
-    local Job = require("plenary.job")
     local filename = vim.fn.shellescape(vim.api.nvim_buf_get_name(0))
     filename = string.sub(filename, 2, -2)
     vim.cmd("let tobedeleted = bufnr('%') | b# | exe \"bd! \" . tobedeleted")
 
-    Job:new({
-      command = "sioyek",
-      args = { filename },
-    }):start()
+    local Job = require("plenary.job")
+    local job = { command = "", args = { filename } }
+    if string.match(filename, "%.pdf$") then
+      job.command = "sioyek"
+    elseif string.match(filename, "%.png$") then
+      job.command = "qlmanage"
+      job.args = { "-p", filename }
+    else
+      return
+    end
+
+    Job:new(job):start()
   end,
 })
