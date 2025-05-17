6b94b8959d9f783a9a822f59556c6167b6fb4803
Soros Liu <soros.liu1029@gmail.com>
2025-05-17 13:54:13 +0800

    update stow.sh

---
+++ stow.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+  Linux*) machine=Linux ;;
+  Darwin*) machine=Mac ;;
+  *)
+    echo "Unknown Machine:${unameOut}"
+    exit 0
+    ;;
+esac
+
+if [[ $machine == "Linux" ]]; then
+  stow -vv --dotfiles stow bash btop conda gh gh-copilot git p10k lazygit nvim \
+    tmux vim zsh
+elif [[ $machine == "Mac" ]]; then
+  stow -vv --dotfiles stow bash btop conda doom gh gh-copilot git gnupg lazygit nvim \
+    p10k sioyek spotify-player tmux vim zsh
+fi
