797ff64cfb32f4d2ba8e4b2f8fbac6b05871ddfc
Soros Liu <soros.liu1029@gmail.com>
2025-08-28 16:23:08 -0400

    add vimtex plugin to support latex files

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -7,7 +7,11 @@ return {
         ensure_installed = require("configs.treesitter.parsers"),
         auto_install = true,
         sync_install = false,
-        highlight = { enable = true },
+        highlight = {
+          enable = true,
+          disable = { "latex" },
+          additional_vim_regex_highlighting = { "latex", "markdown" },
+        },
         indent = { enable = true },
       })
     end,
